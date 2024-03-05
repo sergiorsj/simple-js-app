f9d7382cb1675f3c7e59a031621c0c7a34020eb7
sergiorsj11 <sergiorsj11@gmail.com>
2024-03-04 23:37:26 -0500

    js
    some adjustments

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
-let pokemonList = [{name: 'Bulbasaur', height: '71', type: 'grass'},
-{name: 'Charmander', height: '61', type: 'fire'},
-{name: 'Squirtle', height: '51', type: 'water'}];
+// height = centimeters
+
+var pokemonList = []
+
+    pokemonList.push({name: 'Bulbasaur', height: 71, type: ['grass' , 'poison']});
+    pokemonList.push({name: 'Charmander', height: 61, type: 'fire'});
+    pokemonList.push({name: 'Squirtle', height: 51, type: 'water'});
+
+for (let i = 0; i < pokemonList.length; i++){
+    var item = pokemonList[i];
+    console.log(item.name + ' - height: ' + item.height);
+    document.write(pokemonList[i]);
+}
