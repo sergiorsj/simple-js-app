ab414ea2e282dc174e6c1b07b25e0dc25f7b2ea1
sergiorsj11 <sergiorsj11@gmail.com>
2024-03-07 21:01:35 -0500

    Update scripts.js
    using forEach() loop exerc 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,16 @@ var pokemonList = []
     pokemonList.push({name: 'Charmander', height: 61, type: 'fire'});
     pokemonList.push({name: 'Squirtle', height: 51, type: 'water'});
 
-for (let i = 0; i < pokemonList.length; i++){
-    var item = pokemonList[i];
-    document.write(pokemonList[i].name + ' height: ' + pokemonList[i].height + '<br>');
-}
+//for (let i = 0; i < pokemonList.length; i++){
+//    var item = pokemonList[i];
+//    document.write(pokemonList[i].name + ' height: ' + pokemonList[i].height + '<br>');
+//}
+
+// part 1 exerc 1.5 - forEach() Loops
+
+pokemonList.forEach(pokemon => {
+    console.log('name: ${pokemon.name}, height: ${pokemon.height}, type: ${pokemon.type}');
+});
 
 const heightThreshold = 70;
 
