15401b6981ccc8ae301f698b52fc6aa949b28dee
sergiorsj11 <sergiorsj11@gmail.com>
2024-03-13 21:27:12 -0400

    exerc 1.8
    follow instructions and try to meet them

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,51 @@
 let pokemonRepository = (function () {
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
+    function showModal(title, text) {
+      let modalContainer = document.querySelector('modal-container');
+      modalContainer.innerHTML = '';
+      let modal = document.createElement('div');
+        modal.classList.add('modal');
+      let closeButtonElement = document.createElement('button');
+      closeButtonElement.classList.add('modal-close');
+      closeButtonElement.innerText = 'Close';
+      closeButtonElement.addEventListener('click', hideModal);
+      let titleElement = document.createElement('h1');
+      titleElement.innerText = title;
+      let contentElement = document.createElement('p');
+      contentElement.innerText = text;
+
+      modal.appendChild(closeButtonElement);
+      modal.appendChild(titleElement);
+      modal.appendChild(contentElement);
+      modalContainer.appendChild(modal);
+
+
+      modalContainer.classList.add('is-visible');
+    }
+
+    function hideModal() {
+      let modalContainer = document.querySelector('#modal-container');
+      modalContainer.classList.remove('is-visible');
+    }
+
+    window.addEventListener('keydown', (e) => {
+      let modalContainer = document.querySelector('#modal-container');
+      if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+        hideModal();
+    }
+  });
+  let modalContainer = document.querySelector('#modal-container');
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
+    document.querySelector('#show-modal').addEventListener('click', () => {showModal();
+    });
   
     function add(pokemon) {
       if (
